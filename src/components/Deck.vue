472033e2eb634fd2dd72b91cd704e402043533ea
Christina Li <ckli2@andrew.cmu.edu>
2021-06-17 14:22:43 -0400

    Added style on click & some correctness methods in Deck

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,7 +1,10 @@
 <template>
     <img src="../assets/transparent-card.png" 
-    v-bind:style="{ left:x+'%', top:y+'%' }">
-    <!-- <p> {{ x }}, {{ y }} </p> -->
+    @click="addCardToDeck(topCard)"
+    :style="clicked ? { left:x+'%', top:y+'%', backgroundColor:'yellow' } : { left:x+'%', top:y+'%' }">
+    <!-- <p> {{ x }}, {{ y }} </p>
+     v-bind:style="{ left:x+'%', top:y+'%' }">
+     -->
    
 </template>
 
@@ -11,7 +14,33 @@ export default {
     name: 'Deck',
     props: {
         x: Number,
-        y: Number
+        y: Number,
+        topCard: String
+    },
+    data (){
+        return {
+            clicked: false
+        }
+    },
+    methods: {
+        deckMatch(deckCard, clickedCard){
+            if (true){
+                this.$parent.moveCard(clickedCard, deckCard);
+            }
+        },
+        addCardToDeck(top){
+            this.clicked = true;
+            if (!this.$parent.isOneClicked){
+                console.log('No other card clicked');
+                this.clicked = false;
+                return;
+            }
+            else {
+                const clickedSuit = this.$parent.currClickedSuit;
+                // Actually should not be suit, should be key/ref
+                deckMatch(top, clickedSuit);
+            }
+        }
     }
 }
 </script>
