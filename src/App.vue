5e6963495dbbef8a969adad687189c6f430aaf43
Christina Li <ckli2@andrew.cmu.edu>
2021-06-17 13:39:30 -0400

    Added card data

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,25 @@
   <!--  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <h1> solitaire </h1>
-  <Card col="1" suit="" color=""/>
-  <Card col="2" suit="" color=""/>
-  <Card col="3" suit="" color=""/>
-  <Card col="4" suit="" color=""/>
-  <Card col="5" suit="" color=""/>
-  <Card col="6" suit="" color=""/>
-  <Card col="7" suit="" color=""/>
+  <Card :col="1" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
+  <Card :col="2" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
+  <Card :col="3" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
+  <Card :col="4" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
+  <Card :col="5" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
+  <Card :col="6" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
+  <Card :col="7" :suit="0" color="" :isClicked="false" :isFaceUp="false"/>
 
-  <Deck y="5" x="85"/>
-  <Deck y="28" x="85"/>
-  <Deck y="51" x="85"/>
-  <Deck y="74" x="85"/>
+  <Deck :y="5" :x="85"/>
+  <Deck :y="28" :x="85"/>
+  <Deck :y="51" :x="85"/>
+  <Deck :y="74" :x="85"/>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Card from './components/Card.vue'
 import Deck from './components/Deck.vue'
+import cardData from './data/cards.js'
 export default {
   name: 'App',
   data() {
@@ -27,7 +28,8 @@ export default {
       isOneClicked: false,
       isTwoClicked: false,
       currClickedSuit: '',
-      currClickedColor: ''
+      currClickedColor: '',
+      cards: cardData
     }
   },
   components: {
@@ -37,20 +39,39 @@ export default {
   methods: {
     checkMove(col, suit, color){
       console.log('col clicked: ' + col + ', suit: ' + suit + ', color' + color);
-      this.isOneClicked = !this.isOneClicked;
       if (this.isOneClicked){
-        // isOne = true ==> only one is clicked
-        console.log('One is clicked');
-        this.isTwoClicked = false;
-        console.log('isTwoClicked: ' + this.isTwoClicked);
+        // One is clicked ==> the current click is for the second click
+        this.isOneClicked = false;
+        this.isTwoClicked = true;
+        console.log('Two are now clicked');
       }
       else {
-        // isOne = false ==> it used to be true ==> there had been one clciked
-        // ==> clicking this one makes it 2 clicked ==> isTwo = true
-        console.log('Two are clicked');
-        this.isTwoClicked = true;
-        console.log('isTwoClicked: ' + this.isTwoClicked);
+        // One is NOT clicked ==> either 2 are clicked, or none are clicked
+        if (!this.isTwoClicked){
+          // Two are NOT clicked ==> none are clicked
+          this.isOneClicked = true;
+          console.log('None were clicked, now one is clicked');
+        }
+        else {
+          console.log('Two were clicked; no change');
+          this.isTwoClicked = false;
+        }
       }
+        
+      // this.isOneClicked = !this.isOneClicked;
+      // if (this.isOneClicked){
+      //   // isOne = true ==> only one is clicked
+      //   console.log('One is clicked');
+      //   this.isTwoClicked = false;
+      //   console.log('isTwoClicked: ' + this.isTwoClicked);
+      // }
+      // else {
+      //   // isOne = false ==> it used to be true ==> there had been one clciked
+      //   // ==> clicking this one makes it 2 clicked ==> isTwo = true
+      //   console.log('Two are clicked');
+      //   this.isTwoClicked = true;
+      //   console.log('isTwoClicked: ' + this.isTwoClicked);
+      // }
     }
   }
 }
---
+++ src/data/cards.js
@@ -0,0 +1,64 @@
+const cards = [
+    // Red hearts
+    {color: 'red', suit: 'heart', number: 1},
+    {color: 'red', suit: 'heart', number: 2},
+    {color: 'red', suit: 'heart', number: 3},
+    {color: 'red', suit: 'heart', number: 4},
+    {color: 'red', suit: 'heart', number: 5},
+    {color: 'red', suit: 'heart', number: 6},
+    {color: 'red', suit: 'heart', number: 7},
+    {color: 'red', suit: 'heart', number: 8},
+    {color: 'red', suit: 'heart', number: 9},
+    {color: 'red', suit: 'heart', number: 10},
+    {color: 'red', suit: 'heart', number: 11},
+    {color: 'red', suit: 'heart', number: 12},
+    {color: 'red', suit: 'heart', number: 13},
+    
+    // Red diamonds
+    {color: 'red', suit: 'diamond', number: 1},
+    {color: 'red', suit: 'diamond', number: 2},
+    {color: 'red', suit: 'diamond', number: 3},
+    {color: 'red', suit: 'diamond', number: 4},
+    {color: 'red', suit: 'diamond', number: 5},
+    {color: 'red', suit: 'diamond', number: 6},
+    {color: 'red', suit: 'diamond', number: 7},
+    {color: 'red', suit: 'diamond', number: 8},
+    {color: 'red', suit: 'diamond', number: 9},
+    {color: 'red', suit: 'diamond', number: 10},
+    {color: 'red', suit: 'diamond', number: 11},
+    {color: 'red', suit: 'diamond', number: 12},
+    {color: 'red', suit: 'diamond', number: 13},
+    
+    // Black clubs
+    {color: 'black', suit: 'clubs', number: 1},
+    {color: 'black', suit: 'clubs', number: 2},
+    {color: 'black', suit: 'clubs', number: 3},
+    {color: 'black', suit: 'clubs', number: 4},
+    {color: 'black', suit: 'clubs', number: 5},
+    {color: 'black', suit: 'clubs', number: 6},
+    {color: 'black', suit: 'clubs', number: 7},
+    {color: 'black', suit: 'clubs', number: 8},
+    {color: 'black', suit: 'clubs', number: 9},
+    {color: 'black', suit: 'clubs', number: 10},
+    {color: 'black', suit: 'clubs', number: 11},
+    {color: 'black', suit: 'clubs', number: 12},
+    {color: 'black', suit: 'clubs', number: 13},
+    
+    // Black spades
+    {color: 'black', suit: 'spades', number: 1},
+    {color: 'black', suit: 'spades', number: 2},
+    {color: 'black', suit: 'spades', number: 3},
+    {color: 'black', suit: 'spades', number: 4},
+    {color: 'black', suit: 'spades', number: 5},
+    {color: 'black', suit: 'spades', number: 6},
+    {color: 'black', suit: 'spades', number: 7},
+    {color: 'black', suit: 'spades', number: 8},
+    {color: 'black', suit: 'spades', number: 9},
+    {color: 'black', suit: 'spades', number: 10},
+    {color: 'black', suit: 'spades', number: 11},
+    {color: 'black', suit: 'spades', number: 12},
+    {color: 'black', suit: 'spades', number: 13}
+    
+    ]
+    
+    export default cards;
